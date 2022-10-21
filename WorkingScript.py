1084e3585adb22e229b3bbbdae26fc62b2b44f1a
Ahmet Zamanis <ahmetzamanis@gmail.com>
2022-10-21 18:28:28 +0300

    Split holidays data

---
+++ WorkingScript.py
@@ -0,0 +1,139 @@
+#KAGGLE STORE SALES FORECASTING COMPETITION
+
+import pandas as pd
+
+
+
+
+#LOAD DATASETS
+
+
+#training and testing time series
+df_train = pd.read_csv("./OriginalData/train.csv", encoding="utf-8")
+df_train.shape
+#(3000888, 6)
+
+df_test = pd.read_csv("./OriginalData/test.csv", encoding="utf-8")
+
+#stores data
+df_stores = pd.read_csv("./OriginalData/stores.csv", encoding="utf-8")
+
+#oil price data
+df_oil = pd.read_csv("./OriginalData/oil.csv", encoding="utf-8")
+
+#holidays data
+df_holidays = pd.read_csv("./OriginalData/holidays_events.csv", encoding="utf-8")
+df_holidays.date.nunique()
+#there are duplicate dates in holidays
+
+
+#data dictionary
+
+#df_train, df_test
+df_train.head()
+df_test.head()
+  #date: date in year, month, day
+  #sales: sales that day
+  #onpromotion: n. of items in the product family on promotion, at given date
+  #family: category of product sales
+  #store_nbr: store number (categorical)
+
+
+#df_oil
+df_oil.head()
+  #oil prices by date
+
+
+#df_stores
+df_stores.head()
+  #store_nbr: store number (categorical)
+  #city: 22 cities
+  #state: 16 states
+  #type: type of store (5 types)
+  #cluster: 17 clusters
+  
+
+#df_holidays
+df_holidays.head()
+  #type: 'Holiday', 'Transfer', 'Additional', 'Bridge', 'Work Day', 'Event'
+    #Holiday: de jure holiday, de facto holiday if Transferred=False
+    #Transfer: de jure no holiday, de facto holiday
+    #Additional: de jure holiday, de facto holiday (additional days for a 1+ day holiday)
+    #Bridge: de jure no holiday, de facto holiday (awarded to extend holidays)
+    #Work Day: de jure holiday, de facto no holiday (detracted to make up for bridges)
+  #locale: Local, Regional, National
+  #locale_name: 23 different cities/regions, or Ecuador
+  #description: 103 different holidays
+
+
+#rename holiday_types column to avoid clashes at merge
+df_holidays.rename(columns={"type":"holiday_type"}, inplace=True)
+
+
+#inspect unique date and duplicate date holidays
+df_holidays_unique = df_holidays.drop_duplicates(subset="date", keep=False)
+df_holidays_duplicate = df_holidays[df_holidays.duplicated(["date"], keep=False)]
+
+
+#add calendar_holiday and actual_holiday columns
+  #calendar_holiday=1 if: Holiday, Additional, Work Day
+  #actual_holiday=1 if: (Holiday & Transferred=FALSE), Transfer, Additional, Bridge
+df_holidays["calendar_holiday"] = df_holidays.holiday_type.isin(["Holiday", "Additional", "Work Day"])
+df_holidays["actual_holiday"] = (
+  df_holidays.holiday_type.isin(["Transfer", "Additional", "Bridge"]) | 
+((df_holidays.holiday_type=="Holiday") & (df_holidays.transferred==False))
+)
+
+
+#split holidays into local, regional, national
+df_local = df_holidays.loc[df_holidays.locale=="Local"]
+df_regional = df_holidays.loc[df_holidays.locale=="Regional"]
+df_national = df_holidays.loc[df_holidays.locale=="National"]
+
+
+
+
+
+
+
+#MERGE / JOIN DATASETS
+
+
+#merge stores columns into train and test, by store_nbr
+df_test = df_test.merge(df_stores, how="left", left_on="store_nbr", right_on="store_nbr")
+df_train = df_train.merge(df_stores, how="left", left_on="store_nbr", right_on="store_nbr")
+
+
+#set indexes to date
+df_train.set_index(pd.PeriodIndex(df_train.date, freq="D"), inplace=True)
+df_train.drop("date", axis=1, inplace=True)
+
+df_test.set_index(pd.PeriodIndex(df_test.date, freq="D"), inplace=True)
+df_test.drop("date", axis=1, inplace=True)
+
+df_oil.set_index(pd.PeriodIndex(df_oil.date, freq="D"), inplace=True)
+df_oil.drop("date", axis=1, inplace=True)
+
+df_holidays.set_index(pd.PeriodIndex(df_holidays.date, freq="D"), inplace=True)
+df_holidays.drop("date", axis=1, inplace=True)
+
+
+#join oil price into train and test, by date index
+df_oil.rename(columns={"dcoilwtico":"oil"}, inplace=True)
+df_train = df_train.join(df_oil, how="left", on="date")
+df_test = df_test.join(df_oil, how="left", on="date")
+
+
+#join holidays columns except description into train and test
+df_holidays.drop("description", axis=1, inplace=True)
+df_test = df_test.join(df_holidays, how="left", on="date")
+df_train2 = df_train.merge(df_holidays, how="left", on="date")
+df_train_added = pd.concat([df_train, df_train2]).drop_duplicates(keep=False)
+
+
+#remove supplementary dataframes
+del df_holidays, df_oil, df_stores
+
+
+
+
